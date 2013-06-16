d8d45e7ae15ef243d5c9c68744db73c2ae5fbe83
rlbisbe <rober90@gmail.com>
2013-06-16 23:56:03 +0200

    Added demo app and first description

---
+++ DemoApp/API/PlasticProgram.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoApp.API
+{
+    class PlasticProgram
+    {
+        public static List<Repository> ListRepositories()
+        {
+            Process p = new Process();
+            p.StartInfo.FileName = "cm";
+            p.StartInfo.Arguments = "lrep";
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.UseShellExecute = false;
+            p.Start();
+
+            List<Repository> repositories = new List<Repository>();
+            while (!p.StandardOutput.EndOfStream)
+            {
+                string line = p.StandardOutput.ReadLine();
+                repositories
+                    .Add(RepositoryFactory.RepositoryFromString(line));
+            }
+
+            return repositories;
+        }
+    }
+}
---
+++ DemoApp/API/RepositoryFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace DemoApp.API
+{
+    class RepositoryFactory
+    {
+        public static Repository RepositoryFromString(string line)
+        {
+            Regex regex = new Regex(@"(\S*) (\S*) (\S*)");
+            Match match = regex.Match(line);
+            Repository repository = new Repository();
+            repository.Id = match.Groups[2].Value;
+            repository.Name = match.Groups[3].Value;
+            repository.Server = match.Groups[4].Value;
+            return repository;
+        }
+    }
+}
