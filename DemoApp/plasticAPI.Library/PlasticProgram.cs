587a2c22e42c2d60f1dede81cb3dfebe4004b3f9
rlbisbe <rober90@gmail.com>
2013-06-20 00:42:55 +0200

    Added library, and test project.
    2 tests running by now.

---
+++ DemoApp/plasticAPI.Library/PlasticProgram.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlasticAPI.Library
+{
+    public class PlasticProgram
+    {
+        /// <summary>
+        /// Lists all repositories on the current machine
+        /// TODO: Extend to ask for other servers
+        /// </summary>
+        /// <returns>List of repositories</returns>
+        public static List<Repository> ListRepositories()
+        {
+            Process p = new Process();
+            p.StartInfo.FileName = "cm";
+            p.StartInfo.Arguments = "lrep";
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.UseShellExecute = false;
+            p.Start();
+
+            List<Repository> repositories = new List<Repository>();
+            while (!p.StandardOutput.EndOfStream)
+            {
+                string line = p.StandardOutput.ReadLine();
+                repositories
+                    .Add(RepositoryFactory.RepositoryFromString(line));
+            }
+
+            return repositories;
+        }
+
+        /// <summary>
+        /// Creates a new repository with the specified information
+        /// </summary>
+        /// <param name="rep">Repository to create</param>
+        public static void CreateRepository(Repository rep)
+        {
+            if (string.IsNullOrEmpty(rep.Server))
+                throw new ArgumentException("Repository server cannot be null");
+
+            if (string.IsNullOrEmpty(rep.Name))
+                throw new ArgumentException("Repository name cannot be null");
+                
+            Process p = GetCmProcess();
+            p.StartInfo.Arguments = "mkrep " + rep.Server + " " + rep.Name;
+            p.Start();
+        }
+
+        private static Process GetCmProcess()
+        {
+            Process p = new Process();
+            p.StartInfo.FileName = "cm";
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.UseShellExecute = false;
+            return p;
+        }
+    }
+}
---
+++ plasticAPI.Tests/RepositoryTest.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using PlasticAPI.Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlasticAPI.Tests
+{
+    [TestFixture]
+    public class RepositoryTest
+    {
+        [Test]
+        public void ListRepositoryTest()
+        {
+            List<Repository> repos = PlasticProgram.ListRepositories();
+            Assert.AreEqual("default", repos[0].Name);
+        }
+
+        [Test]
+        public void AddNewRepository()
+        {
+            Repository rep = new Repository();
+            rep.Name = "NewRepository";
+            rep.Server = "localhost:8087";
+            PlasticProgram.CreateRepository(rep);
+            //TODO: Return a specific id.
+        }
+
+    }
+}
